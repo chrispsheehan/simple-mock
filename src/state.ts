daf6977f8d59f05e3625f9b0959d82382206a4df
Chris Sheehan <chris.sheehan@ziglu.io>
2023-01-07 15:30:07 +0000

    convert state to class

--- src/state.ts
+++ src/state.ts
@@ -4,36 +4,46 @@ import { StateObject } from 'stateObject';
 
 const statefile = '../state.json';
 
-const statefilePath = path.join(__dirname, statefile);
 const defaultObj = {};
 
-function exists () {  
+export class State {
 
-    try {
-        fs.accessSync(statefilePath, fs.constants.F_OK);
-        return true;
-    } catch(err) {
-        console.warn(`Could not find state file at ${statefilePath} - have you set permissons?`)
-        return false
+    private statefilePath: string;
+
+    constructor() {
+
+        this.statefilePath = path.join(__dirname, statefile);
+
+        if (!this.exists()) {
+            fs.writeFile(this.statefilePath, JSON.stringify(defaultObj), function (err) {
+                console.log(`State file created at ${this.statefilePath}`);
+            });
+        }
     }
-}
 
-export function getState () {
-    return JSON.parse(fs.readFileSync(statefilePath).toString());
-}
+    exists (): boolean {  
 
-export const saveState = (data: StateObject) => {
+        try {
+            fs.accessSync(this.statefilePath, fs.constants.F_OK);
+            return true;
+        } catch(err) {
+            console.warn(`Could not find state file at ${this.statefilePath} - have you set permissons?`)
+            return false
+        }
+    }
 
-    if (!exists()) {
-        fs.writeFile(statefilePath, JSON.stringify(defaultObj), function (err) {
-            console.log(`State file created at ${statefilePath}`);
+    save = (data: StateObject) => {
+    
+        fs.writeFile(this.statefilePath, JSON.stringify(data), function (err) {
+            if (err) {
+                console.log(err.message);
+                return;
+            }
         });
     }
 
-    fs.writeFile(statefilePath, JSON.stringify(data), function (err) {
-        if (err) {
-            console.log(err.message);
-            return;
-        }
-    });
+    get (): StateObject {
+        
+        return JSON.parse(fs.readFileSync(this.statefilePath).toString());
+    }
 }
