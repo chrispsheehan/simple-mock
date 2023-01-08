26878a4b7e1585acf5d7cd850c685608be0c1f7c
Chris Sheehan <chris.sheehan@ziglu.io>
2023-01-08 14:29:56 +0000

    extend express app func

--- src/routes.state.ts
+++ src/routes.state.ts
@@ -1,16 +1,16 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { StateLoader } from "./stateLoader";
 
 let loader = new StateLoader();
 
-const reset = (req: Request, res: Response) => {
+const reset = (res: Response) => {
 
     loader.reset();
 
     res.status(204).json(loader.state);
 }
 
-const get = (req: Request, res: Response) => {
+const get = (res: Response) => {
 
     loader.load();
     res.status(200).json(loader.state);
