3102c7a7f699f4cd52d7fe0b984e213172033f23
Chris Sheehan <chris.sheehan@ziglu.io>
2023-01-09 15:02:32 +0000

    Auto save state (#2)
    * extend express app func
    
    * fixes
    
    * formatting
    
    * handle blank state file
    
    * remove generic routes
    
    * debug
    
    * pass in state loader
    
    * state class
    
    * tidy
    
    * take out state
    
    * add state class to req obj
    
    * rough working WIP commit :)
    
    * implement state endpoints
    
    * test update file
    
    * add onfinished middlewear
    
    * fixes
    
    * working example
    
    * minor

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,13 @@
-import fs from 'fs';
-import express, { Request, Response } from 'express';
-import https from 'https';
-import http from 'http';
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { mock } from "./mock.app";
+import { Request, Response } from 'express';
 
-import generic from './routes.generic';
-import state from './routes.state';
 import routes from './routes';
 
-const HTTP_PORT = 8080; // standard port
-const HTTPS_PORT = 8443; // standard port
-
-const LOCAL_PORT = process.env.LOCAL_PORT || 2000;
-const MOCK_REFERENCE = process.env.MOCK_REFERENCE || 'ADD_DOTENV_FILE_TO_SET_MOCK_REFERENCE';
-const HTTPS_MODE = process.env.HTTPS_MODE === "true" || false; // workaround to resolve boolean
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use((req: Request, res: Response, next) => { // log out requests
-  console.log(`Params: ${JSON.stringify(req.params)}`); 
-  console.log(`Headers: ${JSON.stringify(req.headers)}`);
-  console.log(`URL: ${JSON.stringify(req.url)}`);
-  next();
-})
-
-app.get('/', (req: Request, res: Response) => {
-  res.send(`Hello from ${MOCK_REFERENCE} Mock!`);  
-});
+const app = mock();
 
 app.get('/health', (req: Request, res: Response) => { 
-  generic.health(req, res);
+  res.json({health: 'OK'});
 });
-
-app.delete('/state', (req: Request, res: Response) => { 
-  state.reset(req, res);
-});
-
-app.get('/state', (req: Request, res: Response) => { 
-  state.get(req, res);
-});
-
-///////////////// MOCK ROUTES ///////////////
 
 app.post('/users', (req: Request, res: Response) => { 
   routes.postUser(req, res);
@@ -53,41 +17,8 @@ app.get('/users', (req: Request, res: Response) => {
   routes.getUsers(req, res);
 });
 
-///////////////// END //////////////////////
-
-
-
 // log out invalid requests
-app.get('/*', function(req: Request, res: Response) {
-  generic.invalid(req, res);
+app.all('/*', function(req: Request, res: Response) {
+  console.error(`***ROUTE NOT SUPPORTED***\n`);
+  res.status(404).json({message: "invalidRoute"})
 });
-
-/// Serves basic localhost site
-var httpServer = http.createServer(app);
-httpServer.listen(HTTP_PORT);
-
-app.listen(LOCAL_PORT, () => {
-  console.log(`${MOCK_REFERENCE} mock up and running!`);
-});
-
-/////// START OF HTTPS CONFIG //////
-if(HTTPS_MODE) { // stop some noise locally
-  const certsFolder = '/dist/certs'; // mount cert files to this folder
-  const keyfile = `${certsFolder}/tls.key`; // most likely saved as a secret somewhere
-  const certfile = `${certsFolder}/tls.crt`; // same for this
-  
-  if (fs.existsSync(keyfile)) {
-    
-    var privateKey  = fs.readFileSync(keyfile, 'utf8');
-    var certificate = fs.readFileSync(certfile, 'utf8');
-    
-    var credentials = {key: privateKey, cert: certificate};
-  
-    var httpsServer = https.createServer(credentials, app);
-  
-    httpsServer.listen(HTTPS_PORT);
-  }
-  else {
-    console.error('!!!COULD NOT FIND KEY/CRT FILES - THIS IS FINE FOR LOCAL DEV!!!')
-  }
-}
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,15 @@
 import { createGuid } from "./helper";
 import { Request, Response } from 'express';
-import { StateLoader } from "./stateLoader";
 
-let loader = new StateLoader();
-
-const usersExist = (): boolean => {
+const usersExist = (state: any): boolean => {
+    
     try {
-        if(loader.state.users.length) {
+        
+        if(Array.isArray(state.users)) {
             return true;
         }
     } catch (error) {
+
         console.warn('user object not found')
         return false;
     }
@@ -17,11 +17,10 @@ const usersExist = (): boolean => {
 
 const getUsers = (req: Request, res: Response) => {
 
-    loader.load();
-
     let userid = req.query.userid;
 
-    if(!usersExist()) {
+    if(!usersExist(state.data)) {
+        
         res.status(200).json({
             users: []
         });
@@ -29,34 +28,35 @@ const getUsers = (req: Request, res: Response) => {
     else {
         if(userid) {
 
-            let user = loader.state.users.filter(user => user.id === userid)
+            let user = global.state.data.users.filter(user => user.id === userid)
             res.status(200).json(user[0]);
         }
         else {
-            res.status(200).json(loader.state.users);
+
+            res.status(200).json(global.state.data.users);
         }
     }
 }
 
 const postUser = (req: Request, res: Response) => {
-    
-    loader.load();
 
     let newUser = req.body;
 
-    if(!usersExist()) { // create users if not there
-        loader.state = {
+    if(!usersExist(global.state.data)) { // create users if not there
+        
+        global.state.data = {
             users: []
         }
     }
 
-    if (loader.state.users.filter((user: { firstName: string; lastName: string; }) => user.firstName === newUser.firstName && user.lastName === newUser.lastName).length > 0) {
+    if (global.state.data.users.filter((user: { firstName: string; lastName: string; }) => user.firstName === newUser.firstName && user.lastName === newUser.lastName).length > 0) {
+        
         res.status(400).json({badrequest: "User already exists"});
     }
     else {
+        
         let savedUser = {...newUser, ...{id: createGuid()}}
-        loader.state.users.push(savedUser);
-        loader.save();
+        global.state.data.users.push(savedUser);
         res.status(201).json(savedUser);
     }
 }
